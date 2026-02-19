578bb0e876dec7635b20b8b2fbc015063c1eb1a2
alicehocane <hereyouth@gmail.com>
2026-02-19 11:58:34 +0500

    update

--- app/enfermedad/[disease]/page.tsx
+++ app/enfermedad/[disease]/page.tsx
@@ -524,7 +524,7 @@ export default async function DiseasePage({ params }: { params: { disease: strin
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-                {CITY_HUBS.map((city) => {
+                {TOP_CITIES.map((city) => {
                     return (
                         <Link 
                             key={city}
