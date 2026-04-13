c32c2844d8248227b08989a71b9df81e3fad08b3
alicehocane <hereyouth@gmail.com>
2026-04-13 15:50:03 +0500

    update

--- components/EncyclopediaCategories.tsx
+++ components/EncyclopediaCategories.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, React } from 'react';
+import React, { useState } from 'react';
 import { ChevronDown, ChevronRight, Activity } from 'lucide-react';
 import Link from 'next/link';
 
